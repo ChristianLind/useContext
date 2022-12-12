1057b851fe86b64a68c952235eabc024b7439012
ChrisLind93 <clind_14@hotmail.com>
2022-12-12 23:48:40 +0100

    useContext

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../userContext';
+import { login } from './../utils/login';
+
+export function Index() {
+  const { user, setUser } = useContext(UserContext);
+
+  return (
+    <div>
+        <h2>Home</h2>
+        <pre>{ JSON.stringify(user, null, 2) }</pre>
+        {//if user logged in, show logout button, if logged out, show login button
+        user ? (
+          <button onClick={() => {
+            //call logout
+            setUser(null)
+          }}
+          >logout
+          </button>
+        ) : (
+          <button 
+            onClick={ async () => {
+              const user = await login();
+              setUser(user);
+            }}
+          >login
+          </button>
+        )}
+    </div>
+  )
+}
